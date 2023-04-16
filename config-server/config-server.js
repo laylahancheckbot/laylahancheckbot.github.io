e8f6498a9e625394f5d0c9ffcd001d06c79bed32
laylahancheckbot <130437347+laylahancheckbot@users.noreply.github.com>
2023-04-16 23:12:18 +0700

    Update config-server.js

--- config-server/config-server.js
+++ config-server/config-server.js
@@ -1,2 +1,2 @@
-const server = "https://27e8-2402-800-6115-3e77-6190-afa3-8cfc-b106.ngrok-free.app";
+const server = "https://549e-2402-800-6115-3e77-e4ef-2e89-208b-46cc.ngrok-free.app";
 // const server = "http://localhost";
